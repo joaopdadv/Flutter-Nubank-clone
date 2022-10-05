bb18be26d8361f8ebed90bd6a5d17c56222eb49e
joaopdadv <joaopdadv@gmail.com>
2022-10-05 14:59:09 -0300

    Parte 2

--- Flutter-Projects/nubank_clone/lib/main.dart
+++ Flutter-Projects/nubank_clone/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nubank_clone/pages/homePage.dart';
+import 'package:nubank_clone/pages/home/homePage.dart';
 
 void main() {
   runApp(const MyApp());
--- Flutter-Projects/nubank_clone/lib/pages/home/homePage.dart
+++ Flutter-Projects/nubank_clone/lib/pages/home/homePage.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:nubank_clone/pages/home/model/header.dart';
 import 'package:nubank_clone/utils/colors_standard.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,14 +16,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroundColor,
+      // backgroundColor: backgroundColor,
       appBar: _appBar(),
       body: SingleChildScrollView(
         child: Container(
           color: Colors.white,
-          child: Column(children: const [
-            Text('data'),
-          ]),
+          child: Column(
+            children: const [
+              Header(),
+            ],
+          ),
         ),
       ),
     );
